e303ae00696dc77887de248b983b5111067321c3
Ramon Irala <rirala@garantizar.com.ar>
2024-09-11 19:09:28 -0300

    upload api fix

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -45,8 +45,6 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
                 if (session.user.banned && !bannedUserAllowedRoutes.includes(pathname)) {
                     router.push('/profile/banned');
                 } else if (!session.user.admitted) {
-                    console.log("redirecting to admission")
-                    console.log(pathname)
                     if (!pendingAdmissionRoutes.includes(pathname)) {
                         router.push('/profile/admission');
                     }
