c26fbbf8e61ad81c81cab4a9985ed5d3bf69ef96
Ramon Irala <rirala@garantizar.com.ar>
2024-09-11 17:19:09 -0300

    PendingAdmissionPage fixed

--- src/app/profile/admission/page.tsx
+++ src/app/profile/admission/page.tsx
@@ -2,7 +2,7 @@ import getServerSession from "@/lib/getServerSession"
 import { ForceBack } from "@/components/ForceBack";
 
 
-export default async function BannedProfile() {
+export default async function PendingAdmissionPage() {
     const session = await getServerSession();
 
     if (!session || session.user.admitted) {
@@ -12,13 +12,12 @@ export default async function BannedProfile() {
     return (
         <div className="flex items-center justify-center h-full">
             <div className="bg-[#222222] p-8 rounded-lg text-center">
-                <div className="text-4xl font-bold mb-4">¡Lo sentimos!</div>
-                <div className="text-lg mb-4">Tu cuenta ha sido bloqueada.</div>
+                <div className="text-4xl font-bold mb-4">¡Gracias por registrarte!</div>
+                <div className="text-lg mb-4">Tu cuenta está pendiente de admisión.</div>
                 <div className="text-sm text-[#cccccc] mb-4">
-                    Por favor, contacta con el administrador si crees que esto es un error.
+                    Por favor, espera a que un administrador revise y apruebe tu solicitud.
                 </div>
             </div>
         </div>
     )
 }
-
