ff07cd30b1308c7448d221f8ce55ceb1e67d2c16
Ramon Irala <rirala@garantizar.com.ar>
2024-09-13 21:24:33 -0300

    BetaWall won't block static files

--- src/app/BetaWall.tsx
+++ src/app/BetaWall.tsx
@@ -4,15 +4,30 @@ import { PRODUCTION_READY } from "@/constants";
 import { useAuth } from "@/contexts/AuthProvider";
 import { redirect, usePathname } from "next/navigation";
 
+const staticFiles = [
+    "/favicon.ico",
+    "/icon.png",
+    "/android-chrome-192x192.png",
+    "/android-chrome-512x512.png",
+    "/apple-touch-icon.png",
+    "/favicon-16x16.png",
+    "/favicon-32x32.png",
+    "/favicon.ico",
+    "/manifest.json",
+    "/robots.txt",
+    "/site.webmanifest",
+    "/sitemap.xml",
+    "/robots.txt",
+];
+const validPathnames = ["/beta", "/home", "/"].concat(staticFiles);
+
 export const BetaWall = () => {
     const { user } = useAuth();
     const pathname = usePathname();
 
-    const validPathnames = ["/beta", "/home", "/"];
 
-    if (!PRODUCTION_READY && !validPathnames.includes(pathname) && (!user || !user.admitted)) {
+    if (PRODUCTION_READY) return null;
+    if (!validPathnames.includes(pathname) && (!user || !user.admitted)) {
         redirect("/beta");
     }
-
-    return null;
 }
